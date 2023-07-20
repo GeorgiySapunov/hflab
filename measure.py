b5d0b41b9a8121895cefa59ffc52fc9ae70dfc66
Georgiy Sapunov <georgiysapunov@yandex.ru>
2023-07-21 00:59:24 +0800

    pm100usb

--- measure.py
+++ measure.py
@@ -4,7 +4,7 @@
 # Thorlabs PM100USB Power and energy meter
 
 # import sys
-# import time
+import time
 import pyvisa
 import numpy as np
 import pandas as pd
@@ -15,9 +15,24 @@ df = pd.DataFrame()
 # choose GPIB Channel 23 as Drain-Source
 rm = pyvisa.ResourceManager()
 rm.list_resources()
+
+# check visa address
+for addr in rm.list_resources():
+    try:
+        print(addr, '-->', rm.open_resource(addr).query('*IDN?').strip())
+    except visa.VisaIOError:
+        pass
+
+input() # check visa
+
 Keysight_B2901A = rm.open_resource('GPIB0::23::INSTR')
+PM100USB = rm.open_resource('') # TODO input
+
 Keysight_B2901A.write("*RST") # The initial settings are applied by the *RST command
 Keysight_B2901A.timeout = 50 # units: ms
+
+PM100USB.write('sense:corr:wav ' + srt(1550)) # set wavelengh
+PM100USB.write('power:dc:unit W')
 
 # Keysight_B2901A.write("")
 
@@ -28,24 +43,37 @@ Keysight_B2901A.write(":SENS:VOLT:PROT 10") # Setting the Limit/Compliance Value
 Keysight_B2901A.write(":OUTP ON") # Measurement channel is enabled by the :OUTP ON command.
 
 Keysight_B2901A.query("*OPC?") # synchronization
-# Keysight_B2901A.write("")
+
+max_power = 0
 
 for i in range(0, 0.05, 0.00001):
     Keysight_B2901A.write(":SOUR:CURR " + str(i)) # Outputs i mA immediately
     Keysight_B2901A.query("*OPC?") # synchronization
-    voltage = Keysight_B2901A.query_ascii_values("MEAS:VOLT?")
+    voltage = float(Keysight_B2901A.query_ascii_values("MEAS:VOLT?"))
     Keysight_B2901A.query("*OPC?") # synchronization
-    current = Keysight_B2901A.query_ascii_values("MEAS:CURR?")
+    current = float(Keysight_B2901A.query_ascii_values("MEAS:CURR?"))
     Keysight_B2901A.query("*OPC?") # synchronization
-    current = Keysight_B2901A.query_ascii_values("MEAS:CURR?")
-    Keysight_B2901A.query("*OPC?") # synchronization
+    power = float(PM100USB.query('measure:power?'))
+    PM100USB.query("*OPC?") # synchronization
+    if power > max_power:
+        max_power = power
+
 
     df.lock[i]["Voltage, V"] = voltage
     df.lock[i]["Current, V"] = current*1000
+    df.lock[i]["Power, W"] = power
 
     print(f"curret set at {i*1000} mA, current: {current*1000} mA, voltage: {voltage}")
-    if float(current) != float(i):
+    if current != float(i):
         print("WARNING: i is not equal to current!")
+
+    if power < max_power*0.8 and i > 0.0003:
+        break
+
+# slowly decrease current
+for i in range(current, 0, 0.0001):
+    Keysight_B2901A.write(":SOUR:CURR " + str(i)) # Outputs i mA immediately
+    time.sleep(0.2)
 
 Keysight_B2901A.write(":OUTP OFF") # Measurement is stopped by the :OUTP OFF command.
 
