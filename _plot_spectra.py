de178530ad08a358b2e5b1114692001bbbc4ede1
Georgiy Sapunov <georgiysapunov@yandex.ru>
2023-12-13 17:09:13 +0800

    2023 Dec 13

--- _plot_spectra.py
+++ _plot_spectra.py
@@ -4,21 +4,63 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# list_of_files = [
+#     "PW2024-1550nm-57M6GSG-25.0°C-20231114-074029-YOKOGAWA_AQ6370D-OS.csv",
+#     "PW2024-1550nm-57M6GSG-35.0°C-20231114-075221-YOKOGAWA_AQ6370D-OS.csv",
+#     # 47m6gsg
+#     "PW2024-1550nm-57M6GSG-45.0°C-20231130-045357-YOKOGAWA_AQ6370D-OS.csv",
+#     "PW2024-1550nm-57M6GSG-55.0°C-20231130-050235-YOKOGAWA_AQ6370D-OS.csv",
+#     # "PW2024-1550nm-57M6GSG-65.0°C-20231130-051353-YOKOGAWA_AQ6370D-OS.csv",
+#     "PW2024-1550nm-57M6GSG-65.0°C-20231130-052227-YOKOGAWA_AQ6370D-OS.csv",
+#     # "PW2024-1550nm-57M6GSG-75.0°C-20231130-053127-YOKOGAWA_AQ6370D-OS.csv",
+#     # 57m6gsg
+#     # "PW2024-1550nm-57M6GSG-45.0°C-20231114-080258-YOKOGAWA_AQ6370D-OS.csv",
+#     # "PW2024-1550nm-57M6GSG-55.0°C-20231114-081326-YOKOGAWA_AQ6370D-OS.csv",
+#     # "PW2024-1550nm-57M6GSG-65.0°C-20231115-073447-YOKOGAWA_AQ6370D-OS.csv",
+#     "PW2024-1550nm-57M6GSG-75.0°C-20231114-083322-YOKOGAWA_AQ6370D-OS.csv",
+#     "PW2024-1550nm-57M6GSG-85.0°C-20231114-084201-YOKOGAWA_AQ6370D-OS.csv",
+# ]
+# list_of_temp = [25, 35, 45, 55, 65, 75, 85]
+
+# list_of_currents = [
+#     [3, 4, 5, 8, 10, 12, 15, 18],
+#     [15],
+#     [15],
+#     [15],
+#     [15],
+#     [15],
+#     [],
+# ]
+
+# # 47M6GSG
+# list_of_files = [
+#     "spb1-1550nm-47M6GSG-25.0°C-20231212-122155-YOKOGAWA_AQ6370D-OS.csv",
+#     "spb1-1550nm-47M6GSG-35.0°C-20231212-123423-YOKOGAWA_AQ6370D-OS.csv",
+#     "spb1-1550nm-47M6GSG-45.0°C-20231212-124321-YOKOGAWA_AQ6370D-OS.csv",
+#     "spb1-1550nm-47M6GSG-55.0°C-20231212-125526-YOKOGAWA_AQ6370D-OS.csv",
+#     "spb1-1550nm-47M6GSG-65.0°C-20231212-130505-YOKOGAWA_AQ6370D-OS.csv",
+# ]
+# list_of_temp = [25, 35, 45, 55, 65, 75, 85]
+
+# list_of_currents = [
+#     [3, 4, 5, 8, 10, 12, 15, 18],
+#     [15],
+#     [15],
+#     [15],
+#     [15],
+# ]
+
+# directory = "data/spb1-1550nm/47M6GSG/OSA/"
+# spectra_xlim_left = 1563
+# spectra_xlim_right = 1577
+
+# 47M6GSGlens
 list_of_files = [
-    "PW2024-1550nm-57M6GSG-25.0°C-20231114-074029-YOKOGAWA_AQ6370D-OS.csv",
-    "PW2024-1550nm-57M6GSG-35.0°C-20231114-075221-YOKOGAWA_AQ6370D-OS.csv",
-    # 47m6gsg
-    "PW2024-1550nm-57M6GSG-45.0°C-20231130-045357-YOKOGAWA_AQ6370D-OS.csv",
-    "PW2024-1550nm-57M6GSG-55.0°C-20231130-050235-YOKOGAWA_AQ6370D-OS.csv",
-    # "PW2024-1550nm-57M6GSG-65.0°C-20231130-051353-YOKOGAWA_AQ6370D-OS.csv",
-    "PW2024-1550nm-57M6GSG-65.0°C-20231130-052227-YOKOGAWA_AQ6370D-OS.csv",
-    # "PW2024-1550nm-57M6GSG-75.0°C-20231130-053127-YOKOGAWA_AQ6370D-OS.csv",
-    # 57m6gsg
-    # "PW2024-1550nm-57M6GSG-45.0°C-20231114-080258-YOKOGAWA_AQ6370D-OS.csv",
-    # "PW2024-1550nm-57M6GSG-55.0°C-20231114-081326-YOKOGAWA_AQ6370D-OS.csv",
-    # "PW2024-1550nm-57M6GSG-65.0°C-20231115-073447-YOKOGAWA_AQ6370D-OS.csv",
-    "PW2024-1550nm-57M6GSG-75.0°C-20231114-083322-YOKOGAWA_AQ6370D-OS.csv",
-    "PW2024-1550nm-57M6GSG-85.0°C-20231114-084201-YOKOGAWA_AQ6370D-OS.csv",
+    "spb1-1550nm-47M6GSG-25.0°C-20231211-130146-YOKOGAWA_AQ6370D-OS.csv",
+    "spb1-1550nm-47M6GSG-35.0°C-20231211-132211-YOKOGAWA_AQ6370D-OS.csv",
+    "spb1-1550nm-47M6GSG-45.0°C-20231211-134300-YOKOGAWA_AQ6370D-OS.csv",
+    "spb1-1550nm-47M6GSG-55.0°C-20231211-140126-YOKOGAWA_AQ6370D-OS.csv",
+    "spb1-1550nm-47M6GSG-65.0°C-20231211-141357-YOKOGAWA_AQ6370D-OS.csv",
 ]
 list_of_temp = [25, 35, 45, 55, 65, 75, 85]
 
@@ -27,12 +69,10 @@ list_of_currents = [
     [15],
     [15],
     [15],
-    [15],
-    [15],
     [],
 ]
 
-directory = "data/PW2024-1550nm/57M6GSG/OSA/"
+directory = "data/spb1-1550nm/47M6GSG-lens/OSA/"
 spectra_xlim_left = 1563
 spectra_xlim_right = 1577
 
@@ -66,7 +106,7 @@ for fn, file in enumerate(list_of_files):
 ax.grid()  # adding grid
 ax.minorticks_on()
 # Adding labels
-# ax.set_xlabel("Wavelength, nm")
+ax.set_xlabel("Wavelength, nm")
 ax.set_ylabel("Intensity, dBm")
 # Adding legend
 # ax.legend(loc=0, prop={"size": 4})
