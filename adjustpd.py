c3e41cdf7b2756ad656f00e96c96d6bb25a93ea7
Georgiy Sapunov <georgiysapunov@yandex.ru>
2023-12-29 01:19:39 +0800

    fix

--- adjustpd.py
+++ adjustpd.py
@@ -7,15 +7,18 @@ import pyvisa
 import numpy as np
 import pyfiglet
 from rich import print
+from configparser import ConfigParser
 
-from settings import settings
 
+config = ConfigParser()
+config.read("config.ini")
+instruments_config = config["INSTRUMENTS"]
 
 wavelength = sys.argv[1]
 
 rm = pyvisa.ResourceManager()
 PM100USB = rm.open_resource(
-    settings["Thorlabs_PM100USB_address"],
+    instruments_config["Thorlabs_PM100USB_address"],
     write_termination="\r\n",
     read_termination="\n",
 )
@@ -27,7 +30,7 @@ while True:
     output_power = float(PM100USB.query("measure:power?")) * 1000  # mW
     time.sleep(0.03)
     os.system("cls" if os.name == "nt" else "clear")
-    power = pyfiglet.figlet_format(f"{output_power:3.6f}", font="epic")
+    power = pyfiglet.figlet_format(f"{output_power:3.6f}", font="doh")
     color = "blue"
     if output_power < 0.01:
         color = "blue"
@@ -38,3 +41,5 @@ while True:
     elif output_power > 1:
         color = "green"
     print(f"[{color}]{power}[/{color}]")
+    print(f"\n"{output_power:3.6f}")
+    print("Ctrl-C to cancel")
